7f62452fb423f47fc528a16cef7e0ad3b354e81f
Ari Maniatis <ari@ish.com.au>
2016-08-30 10:04:23 +1000

    Create off.py

---
+++ watering/function/off.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+
+import subprocess
+import cgi
+
+print "Content-type: text/html"
+print
+
+cmd = ['sudo', '/usr/bin/python2.7', '/var/www/localhost/htdocs/watering/function/water.py', '-o']
+
+proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+for line in proc.stdout:
+    print "<p>" + line + "<p/>"
